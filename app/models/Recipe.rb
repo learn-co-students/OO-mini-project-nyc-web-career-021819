06d1962c28c07b51d9150764f39f5b90e87e3891
David Korff <dkorff12@gmail.com>
2019-02-26 15:05:35 -0500

    Done.

---
+++ app/models/Recipe.rb
@@ -0,0 +1,81 @@
+class Recipe
+
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def recipe_cards
+    RecipeCard.all.select do |recipe|
+      recipe.recipe == self
+    end
+  end
+
+  def users
+    recipe_cards.map do |recipe|
+      recipe.user
+    end
+  end
+
+  def self.most_popular
+      recipe_arr = []
+      RecipeCard.all.each do |recipe|
+        recipe_arr << recipe.recipe
+      end
+
+      recipe_hash = {}
+      recipe_arr.each do |recipe|
+        if recipe_hash.has_key?(recipe.name)
+          recipe_hash[recipe.name] += 1
+        else
+          recipe_hash[recipe.name] = 1
+        end
+      end
+      most_popular = recipe_hash.sort_by do |recipe, count|
+        count
+      end.last.first
+
+      recipe_arr.find do |recipe|
+        recipe.name == most_popular
+      end
+  end
+
+  def recipe_ingredients
+    RecipeIngredient.all.select do |recipe|
+      recipe.recipe == self
+    end
+  end
+
+  def ingredients
+    recipe_ingredients.map do |recipe|
+      recipe.ingredient
+    end
+  end
+
+  def add_ingredients(ingredients)
+    ingredients.each do |ingredient|
+      RecipeIngredient.new(ingredient, self)
+    end
+    self.ingredients
+  end
+
+  def allergens
+    all_allergens = Allergen.all.select do |allergen|
+      # binding.pry
+      self.ingredients.include?(allergen.ingredient)
+    end
+    all_allergens.map do |allergen|
+      allergen.ingredient
+    end.uniq
+  end
+
+
+end
--- tools/console.rb
+++ tools/console.rb
@@ -1,3 +1,40 @@
 require_relative '../config/environment.rb'
 
+spaghetti_meatballs = Recipe.new("Spaghetti & Meatballs")
+m_and_c = Recipe.new("Mac & Cheese")
+pb_j = Recipe.new("PB&J")
+
+will = User.new("Will")
+matt = User.new("Matt")
+dave = User.new("Dave")
+
+card_1 = RecipeCard.new(will, spaghetti_meatballs, 1.1, 1)
+card_2 = RecipeCard.new(matt, spaghetti_meatballs, 1.1, 2)
+card_3 = RecipeCard.new(will, m_and_c, 1.2, 3)
+card_4 = RecipeCard.new(dave, m_and_c, 1.1, 4)
+card_5 = RecipeCard.new(will, pb_j, 1.2, 5)
+card_6 = RecipeCard.new(matt, pb_j, 1.3, 6)
+card_7 = RecipeCard.new(dave, pb_j, 1.3, 9)
+
+peanut_butter = Ingredient.new("Peanut Butter")
+jelly = Ingredient.new("Jelly")
+macaroni = Ingredient.new("Macaroni")
+cheese = Ingredient.new("Cheese")
+spaghetti = Ingredient.new("Spaghetti")
+meatballs = Ingredient.new("Meatballs")
+
+ing_1 = RecipeIngredient.new(peanut_butter, pb_j)
+ing_2 = RecipeIngredient.new(jelly, pb_j)
+ing_3 = RecipeIngredient.new(macaroni, m_and_c)
+ing_4 = RecipeIngredient.new(cheese, m_and_c)
+ing_5 = RecipeIngredient.new(spaghetti, spaghetti_meatballs)
+ing_6 = RecipeIngredient.new(meatballs, spaghetti_meatballs)
+
+peanut = Allergen.new(dave, peanut_butter)
+lactose = Allergen.new(dave, cheese)
+gluten = Allergen.new(matt, macaroni)
+veg = Allergen.new(will, meatballs)
+peanut_2 = Allergen.new(matt, peanut_butter)
+
 binding.pry
+0
